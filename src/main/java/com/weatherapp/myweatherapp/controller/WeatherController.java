a8aad0b743dc26f4ddd54ca212df9300891ca645
Faiyaz <47196906+faiyaz97@users.noreply.github.com>
2023-06-26 20:47:48 +0100

    simple exceptions handling added

--- src/main/java/com/weatherapp/myweatherapp/controller/WeatherController.java
+++ src/main/java/com/weatherapp/myweatherapp/controller/WeatherController.java
@@ -3,10 +3,12 @@ package com.weatherapp.myweatherapp.controller;
 import com.weatherapp.myweatherapp.model.CityInfo;
 import com.weatherapp.myweatherapp.service.WeatherService;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.client.HttpClientErrorException;
 
 @Controller
 public class WeatherController {
@@ -14,28 +16,39 @@ public class WeatherController {
   @Autowired
   WeatherService weatherService;
 
-  @GetMapping("/forecast/{city}")
-  public ResponseEntity<CityInfo> forecastByCity(@PathVariable("city") String city) {
-
-    CityInfo ci = weatherService.forecastByCity(city);
-
-    return ResponseEntity.ok(ci);
+  @GetMapping("/forecast/")
+  public ResponseEntity<String> forecastWithoutCity() {
+      return new ResponseEntity<>("City name is required", HttpStatus.BAD_REQUEST);
   }
 
-  // TODO: given two city names, compare the length of the daylight hours and return the city with the longest day
+  @GetMapping("/forecast/{city}")
+  public ResponseEntity<?> forecastByCity(@PathVariable("city") String city) {
+    try {
+      CityInfo ci = weatherService.forecastByCity(city);
+      return ResponseEntity.ok(ci);
+    } catch (HttpClientErrorException.BadRequest e) {
+      return new ResponseEntity<>("City not found: " + city, HttpStatus.NOT_FOUND);
+    }
+  }
 
   @GetMapping("/daylight/{city1}/{city2}")
   public ResponseEntity<String> compareDaylightHours(@PathVariable String city1, @PathVariable String city2) {
-    String result = weatherService.compareDaylightHours(city1, city2);
-    return ResponseEntity.ok(result);
+    try {
+      String result = weatherService.compareDaylightHours(city1, city2);
+      return ResponseEntity.ok(result);
+    } catch (HttpClientErrorException.BadRequest e) {
+      return new ResponseEntity<>("One or both cities not found", HttpStatus.NOT_FOUND);
+    }
   }
 
-
-  // TODO: given two city names, check which city its currently raining in
   @GetMapping("/raincheck/{city1}/{city2}")
   public ResponseEntity<String> checkRain(@PathVariable String city1, @PathVariable String city2) {
-    String result = weatherService.checkRain(city1, city2);
-    return ResponseEntity.ok(result);
+    try {
+      String result = weatherService.checkRain(city1, city2);
+      return ResponseEntity.ok(result);
+    } catch (HttpClientErrorException.BadRequest e) {
+      return new ResponseEntity<>("One or both cities not found", HttpStatus.NOT_FOUND);
+    }
   }
 
 
